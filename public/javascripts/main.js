be23323e3c0203f8b54416a5db0f845aa13ee0ed
LAUREN CORFIELD <b4017553@hallam.shu.ac.uk>
2021-02-18 17:54:33 +0000

    fixed tabs main js was not linked properly

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,7 @@ $(window).on("resize", function(ev) {
     $("nav ul").attr("style", "");
   }
 });
+
 function navHighlight(elem, home, active) {
     var url = location.href.split('/'),
         loc = url[url.length -1],
@@ -18,8 +19,11 @@ function navHighlight(elem, home, active) {
             }
         }
     }
-navHighlight('.mainNav ul li a', 'index.html', 'current'); /* menu link selector, home page, highlight class */
+
+    navHighlight('.mainNav ul li a', 'index.html', 'current'); /* menu link selector, home page, highlight class */
 })();
+
+
 function openTab(evt, findTab) {
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -94,6 +98,7 @@ function removeItem() {
 	var item = document.getElementById(helpfulThing.value);
 	ul.removeChild(item);
 }
+
 function changeworkAdd() {
   if ($("#changework tbody").length == 0) {
     $("#changework").append("<tbody></tbody>");
@@ -107,132 +112,4 @@ function changeworkAdd() {
       "<td>15/02/2021</td>"+
       "</tr>");  
 }
-/*var recognitionCanvas = document.getElementById("recognitionCanvas");
-recognitionCanvas.width = 300;
-recognitionCanvas.height = 300;
-   
-var ctx = recognitionCanvas.getContext("2d");
- 
-function drawLine(ctx, startX, startY, endX, endY,color){
-    ctx.save();
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(startX,startY);
-    ctx.lineTo(endX,endY);
-    ctx.stroke();
-    ctx.restore();
-}
- 
-function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
-    ctx.save();
-    ctx.fillStyle=color;
-    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
-    ctx.restore();
-}
- 
-var results = {
-    "Happiness": 10,
-    "Content": 14,
-    "Anxious": 2,
-    "Sad": 12
-};
- 
-var Barchart = function(options){
-    this.options = options;
-    this.canvas = options.canvas;
-    this.ctx = this.canvas.getContext("2d");
-    this.colors = options.colors;
-  
-    this.draw = function(){
-        var maxValue = 0;
-        for (var categ in this.options.data){
-            maxValue = Math.max(maxValue,this.options.data[categ]);
-        }
-        var canvasActualHeight = this.canvas.height - this.options.padding * 2;
-        var canvasActualWidth = this.canvas.width - this.options.padding * 2;
- 
-        //drawing the grid lines
-        var gridValue = 0;
-        while (gridValue <= maxValue){
-            var gridY = canvasActualHeight * (1 - gridValue/maxValue) + this.options.padding;
-            drawLine(
-                this.ctx,
-                0,
-                gridY,
-                this.canvas.width,
-                gridY,
-                this.options.gridColor
-            );
-             
-            //writing grid markers
-            this.ctx.save();
-            this.ctx.fillStyle = this.options.gridColor;
-            this.ctx.textBaseline="bottom"; 
-            this.ctx.font = "bold 10px Arial";
-            this.ctx.fillText(gridValue, 10,gridY - 2);
-            this.ctx.restore();
- 
-            gridValue+=this.options.gridScale;
-        }      
-  
-        //drawing the bars
-        var barIndex = 0;
-        var numberOfBars = Object.keys(this.options.data).length;
-        var barSize = (canvasActualWidth)/numberOfBars;
- 
-        for (categ in this.options.data){
-            var val = this.options.data[categ];
-            var barHeight = Math.round( canvasActualHeight * val/maxValue) ;
-            drawBar(
-                this.ctx,
-                this.options.padding + barIndex * barSize,
-                this.canvas.height - barHeight - this.options.padding,
-                barSize,
-                barHeight,
-                this.colors[barIndex%this.colors.length]
-            );
- 
-            barIndex++;
-        }
- 
-        //drawing series name
-        this.ctx.save();
-        this.ctx.textBaseline="bottom";
-        this.ctx.textAlign="center";
-        this.ctx.fillStyle = "#000000";
-        this.ctx.font = "bold 14px Arial";
-        this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
-        this.ctx.restore();  
-         
-        //draw legend
-        barIndex = 0;
-        var legend = document.querySelector("legend[for='recognitionCanvas']");
-        var ul = document.createElement("ul");
-        legend.append(ul);
-        for (categ in this.options.data){
-            var li = document.createElement("li");
-            li.style.listStyle = "none";
-            li.style.borderLeft = "20px solid "+this.colors[barIndex%this.colors.length];
-            li.style.padding = "5px";
-            li.textContent = categ;
-            ul.append(li);
-            barIndex++;
-        }
-    }
-}
- 
- 
-var myBarchart = new Barchart(
-    {
-        canvas:recognitionCanvas,
-        seriesName:"Vinyl records",
-        padding:20,
-        gridScale:5,
-        gridColor:"#eeeeee",
-        data:myVinyls,
-        colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
-    }
-);
-*/
-
 
